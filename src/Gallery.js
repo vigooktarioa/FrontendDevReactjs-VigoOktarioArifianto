f825c64ec7daba811185714696065ca214af40fe
Laplace <vigooktarioa@gmail.com>
2022-03-24 10:48:27 +0700

    update

--- src/Gallery.js
+++ src/Gallery.js
@@ -116,134 +116,21 @@ class ListGallery extends Component {
                         />
                     ))}
                 </div>
-                {/* <button className="btn btn-success" onClick={() => this.Add()}>
-                    Tambah Data
-                </button> */}
-                {/* component modal sbg control manipulasi data */}
-                <div className="modal" id="modal_buku">
-                    <div className="modal-dialog">
-                        <div className="modal-content">
-                            {/* modal header */}
-                            <div className="modal-header">
-                                Form Buku
-                            </div>
-                            {/* modal body */}
-                            <div className="modal-body">
-                                <form onSubmit={ev => this.Save(ev)}>
-                                    Judul Buku
-                                    <input type="text" className="form-control mb-2"
-                                        value={this.state.judul}
-                                        onChange={ev => this.setState({
-                                            judul:
-                                                ev.target.value
-                                        })}
-                                        required />
-                                    Penulis Buku
-                                    <input type="text" className="form-control mb-2"
-                                        value={this.state.penulis}
-                                        onChange={ev => this.setState({
-                                            penulis
-                                                : ev.target.value
-                                        })}
-                                        required />
-                                    Penerbit Buku
-                                    <input type="text" className="form-control mb-2"
-                                        value={this.state.penerbit}
-                                        onChange={ev => this.setState({ penerbit: ev.target.value })}
-                                        required />
-                                    Harga Buku
-                                    <input type="number" className="form-control mb-2"
-                                        value={this.state.harga}
-                                        onChange={ev => this.setState({
-                                            harga:
-                                                ev.target.value
-                                        })}
-                                        required />
-                                    Cover Buku
-                                    <input type="url" className="form-control mb-2"
-                                        value={this.state.cover}
-                                        onChange={ev => this.setState({
-                                            cover:
-                                                ev.target.value
-                                        })}
-                                        required />
-                                    <button className="btn btn-info btn-block" type="submit">
-                                        Simpan
-                                    </button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
             </div>
         )
-    }
-    Add = () => {
-        // menampilkan komponen modal
-        $("#modal_buku").show(true)
-        this.setState({
-            isbn: Math.random(1, 10000000),
-            judul: "",
-            penulis: "",
-            penerbit: "",
-            cover: "",
-            harga: 0,
-            action: "insert"
-        })
     }
 
     learnMore = (item) => {
         // menampilkan komponen modal
         $("#modal_buku").show()
-        this.setState({
-            nama: item.nama,
-            kategori: item.kategori,
-            penulis: item.penulis,
-            penerbit: item.penerbit,
-            cover: item.cover,
-            harga: item.harga,
-            action: "update",
-            selectedItem: item
+        this.showState({
+            nama:item.nama,
+            kategori:item.kategori,
+            harga:item.harga,
+            bintang:item.bintang,
+            status:item.status
         })
     }
-
-    Save = (event) => {
-        event.preventDefault();
-        // menampung data state buku
-        let tempBuku = this.state.buku
-
-        if (this.state.action === "insert") {
-            // menambah data baru
-            tempBuku.push({
-                isbn: this.state.isbn,
-                judul: this.state.judul,
-                penulis: this.state.penulis,
-                penerbit: this.state.penerbit,
-                cover: this.state.cover,
-                harga: this.state.harga,
-            })
-        } else if (this.state.action === "update") {
-            // menyimpan perubahan data
-            let index = tempBuku.indexOf(this.state.selectedItem)
-            tempBuku[index].isbn = this.state.isbn
-            tempBuku[index].judul = this.state.judul
-            tempBuku[index].penulis = this.state.penulis
-            tempBuku[index].penerbit = this.state.penerbit
-            tempBuku[index].cover = this.state.cover
-            tempBuku[index].harga = this.state.harga
-        }
-
-        this.setState({ buku: tempBuku })
-
-        // menutup komponen modal_buku
-        $("#modal_buku").show(false)
-    }
-
-
-
-    
-
-
 }
 
 
--- src/Kartu.js
+++ src/Kartu.js
@@ -19,45 +19,9 @@ class Kartu extends React.Component {
                             
                         </div>
                     </Card.Body>
-                    <Button className="mt-0" onClick={this.props.learnMore}>Learn More</Button>
+                    <Button className="mt-0" onClick={this.props.learnMore}>LEARN MORE</Button>
 
                 </Card>
-                {/* <div className="card">
-                    <div className="card-body row">
-                        <div className="col-5">
-                            <img src={this.props.cover} className="img"
-                                height="200" />
-                        </div>
-
-                        <div className="col-7">
-                            <h5 className="text-info">
-                                {this.props.nama}
-                            </h5>
-                            <h6 className="text-dark">
-                                ISBN : {this.props.isbn}
-                            </h6>
-                            <h6 className="text-dark">
-                                Penulis: {this.props.penulis}
-                            </h6>
-                            <h6 className="text-dark">
-                                Penerbit: {this.props.penerbit}
-                            </h6>
-                            <h6 className="text-danger">
-                                Harga: Rp {this.props.harga}
-                            </h6>
-
-                            <button className="btn btn-sm btn-primary m-1"
-                                onClick={this.props.onEdit}>
-                                Edit
-                            </button>
-
-                            <button className="btn btn-sm btn-danger m-1"
-                                onClick={this.props.onDrop}>
-                                Hapus
-                            </button>
-                        </div>
-                    </div>
-                </div> */}
                 
             </div>
         )
